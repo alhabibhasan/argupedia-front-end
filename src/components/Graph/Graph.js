8e09d0edff8853f2357ba8abb9cabd8fd9ac2e05
Muhammed Hasan <muhammedahasan@outlook.com>
2019-10-23 22:16:57 +0100

    Update graph component to show a wip graph

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,51 +1,38 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
 import { ForceGraph2D } from 'react-force-graph';
+import { getArgumentChain } from '../../data/api/Api'
 
+import Loading from '../Loading/Loading'
 
 /* Component */
 const Graph = (props) => {
 
-    const [nodes, setNodes] = useState({})
-    const [links, setLinks] = useState({})
+    const [nodes, setNodes] = useState([])
+    const [links, setLinks] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        axios.get("https://gist.githubusercontent.com/mbostock/74cb803c013404ac30e63f020a52a2fd/raw/c7c74c939b602c56c80848963f9ad24802baaead/graph.json")
-            .then(res => {
-                let data = res.data;
-                setLinks(data.links);
-                setNodes(data.nodes);
-            })
+        getArgumentChain()
+        .then(chain => {
+            setNodes(chain.argChain.nodes)
+            setLinks(chain.argChain.links)
+            setLoading(false)
+        })
     }, [])
 
+    const renderGraph = () => {
+        return (
+            <ForceGraph2D 
+                graphData={{nodes,links}}
+                nodeLabel="statement"
+                linkLabel="type"
+            />
+        )
+    }
 
     return (
         <div>
-            <ForceGraph2D
-                graphData={
-                    {
-                        "nodes": [ 
-                            { 
-                              "id": "id1",
-                              "name": "name1",
-                              "val": 1 
-                            },
-                            { 
-                              "id": "id2",
-                              "name": "name2",
-                              "val": 1
-                            }
-                        ],
-                        "links": [
-                            {
-                                "source": "id1",
-                                "target": "id2"
-                            }
-                        ]
-                    }
-                }
-                nodeLabel="name"
-            />,
+            {loading ? <Loading/> : renderGraph()}
         </div>
     )
   }
