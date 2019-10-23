ccc259560b07ac00d600d2fbc70eb4a44051052c
Muhammed Hasan <muhammedahasan@outlook.com>
2019-10-23 22:17:14 +0100

    Get real data from backend

--- src/data/api/Api.js
+++ src/data/api/Api.js
@@ -1,36 +1,12 @@
-const getArgumentData = () => {
-    return {
-        argument: 'This is an argument',
-        upvotes: 1,
-        downvotes: 4,
-        status: 'IN',
-        for: [
-            {
-                argument: 'This is an argument for the argument',
-                upvotes: 1,
-                downvotes: 4,
-                status: 'OUT',
-            },
-            {
-                argument: 'This is another argument for the argument',
-                upvotes: 5,
-                downvotes: 4,
-                status: 'IN',
-            }
-        ],
-        against: [
-            {
-                argument: 'This is an argument against the argument',
-                upvotes: 7,
-                downvotes: 4,
-                status: 'IN',
-            },
-            {
-                argument: 'This is another argument against the argument',
-                upvotes: 10,
-                downvotes: 4,
-                status: 'IN',
-            }
-        ]
-    }
+import axios from 'axios'
+
+const getArgumentChain = () => {
+    return axios.get(process.env.REACT_APP_API_BASE_URL + '/getArgChain')
+        .then(res => {
+            return res.data;
+        })
+}
+
+export {
+    getArgumentChain
 }
