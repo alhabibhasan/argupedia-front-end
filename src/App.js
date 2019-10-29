8f0356ea66ef17713c8ed50c4197fd2d7a500b1f
Muhammed Hasan <muhammedahasan@outlook.com>
2019-10-29 15:35:07 +0000

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import './App.css';
-import Graph from './components/Graph/Graph';
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom"
+
+import './App.css'
+import Index from './pages/Index'
+import RootArgs from './pages/RootArgs'
+import ArgChain from './pages/ArgChain'
 
 function App() {
   return (
-    <div className='App'>
-      Argupedia!
+    <Router>
+      <div className='App'>
+        Argupedia!
 
-      <Graph></Graph>
-    </div>
-  );
+        <nav>
+          <Link to="/">Home</Link>
+          <br/>
+          <Link to="/graphs/roots">Views All Roots</Link>
+        </nav>
+        
+        <Switch>
+          <Route exact path="/graphs/roots" render={(props) => <RootArgs {...props} />}></Route>
+          <Route exact path="/graphs/argument/:id(\d+)" render={(props) => <ArgChain {...props}/>}></Route>
+          <Route exact path="/" ><Index/></Route>
+          <Route render={() => <Redirect to="/" />}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
