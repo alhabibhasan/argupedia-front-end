dc6b4b282eec00848fdaadccb0fe0a94aa966874
Muhammed Hasan <muhammedahasan@outlook.com>
2019-10-29 15:36:04 +0000

    Add api call for root args

--- src/data/api/Api.js
+++ src/data/api/Api.js
@@ -2,11 +2,15 @@ import axios from 'axios'
 
 const getArgumentChain = () => {
     return axios.get(process.env.REACT_APP_API_BASE_URL + '/getArgChain')
-        .then(res => {
-            return res.data;
-        })
+        .then(res => res.data.argChain)
+}
+
+const getArgumentRoots = () => {
+    return axios.get(process.env.REACT_APP_API_BASE_URL + '/getRootArgs')
+        .then(res => res.data.rootArgs)
 }
 
 export {
-    getArgumentChain
+    getArgumentChain,
+    getArgumentRoots
 }
