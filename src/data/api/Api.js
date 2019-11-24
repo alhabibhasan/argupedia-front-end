133a3204630bac4cafa849d3ffe180fb8e36e070
Muhammed Hasan <muhammedahasan@outlook.com>
2019-11-24 20:08:07 +0000

    Add createArgument api function.

--- src/data/api/Api.js
+++ src/data/api/Api.js
@@ -3,14 +3,26 @@ import axios from 'axios'
 const getArgumentChain = (rootId) => {
     return axios.get(process.env.REACT_APP_API_BASE_URL + '/getArgChain/' + rootId)
         .then(res => res.data.argChain)
+        .catch(err => console.log(err))
 }
 
 const getArgumentRoots = () => {
     return axios.get(process.env.REACT_APP_API_BASE_URL + '/getRootArgs')
         .then(res => res.data.rootArgs)
+        .catch(err => console.log(err))
+}
+
+const createArgument = (arg, root) => {
+    arg['root'] = root
+    return axios.post(process.env.REACT_APP_API_BASE_URL+ '/createArg', arg)
+        .then(res => res.data)
+        .catch(err => {
+            throw new Error(err)
+        })
 }
 
 export {
     getArgumentChain,
-    getArgumentRoots
+    getArgumentRoots,
+    createArgument,
 }
