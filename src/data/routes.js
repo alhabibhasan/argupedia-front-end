8b0944ad14c27eadc6ec39786b59c5ed18344e11
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-01 16:43:58 +0000

    Remove redundant d parameter

--- src/data/routes.js
+++ src/data/routes.js
@@ -1,7 +1,7 @@
 const index = { defintion: '/', use: '/'}
 const roots = { defintion: '/roots', use: '/roots'}
 const createArgument = { defintion: '/argument/create', use: '/argument/create'}
-const readArgument = { defintion: '/argument/:id(\d+)', use: '/argument/'}
+const readArgument = { defintion: '/argument/:id', use: '/argument/'}
 
 
 export {
