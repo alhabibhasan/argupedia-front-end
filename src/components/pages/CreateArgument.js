f777487f03f164868ec85795e967c20e1b3cbb8b
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-01 16:44:28 +0000

    Add margin to create arg form.

--- src/components/pages/CreateArgument.js
+++ src/components/pages/CreateArgument.js
@@ -10,45 +10,51 @@ const GuidanceText = styled.div`
     margin: 5%;
 `
 
+const Page = styled.div`
+    margin: 1%;
+`
+
 const CreateArg = (props) => {
     return (
-        <ReactCSSTransitionGroup
-        transitionName="Add-Argument"
-        transitionAppear={true}
-        transitionAppearTimeout={500}
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}>
-            <h1>Let's get started!</h1>
-            <GuidanceText>
-                Arguments posted on Argupedia are required to follow a set structure. The reason for this is to level the playing field 
-                and allow each argument to express their point of view clearly using well defined structures.
+        <Page>
+            <ReactCSSTransitionGroup
+            transitionName="Add-Argument"
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+                <h1>Let's get started!</h1>
+                <GuidanceText>
+                    Arguments posted on Argupedia are required to follow a set structure. The reason for this is to level the playing field 
+                    and allow each argument to express their point of view clearly using well defined structures.
 
-                We are aiming to get a structure which closely resembles the following:
+                    We are aiming to get a structure which closely resembles the following:
 
-            </GuidanceText>
-        
-            <GuidanceText>
-                "In the current circumstance R <br/>
-                We should perform action A <br/>
-                Which will result in new circumstances S <br/>
-                Which will realise goal G <br/>
-                Which will promote some value V." 
+                </GuidanceText>
+            
+                <GuidanceText>
+                    "In the current circumstance R <br/>
+                    We should perform action A <br/>
+                    Which will result in new circumstances S <br/>
+                    Which will realise goal G <br/>
+                    Which will promote some value V." 
 
-                <Tooltip 
-                    text='Replace R, A, S, G and V with some of your points to see if it has a good flow'
-                    style={{color: 'gray'}}
-                    type='info'
-                />
-            </GuidanceText>
-        
-            <GuidanceText>
-                You should also try to give your argument a quick summary/overview in the statement field. This will allow your readers
-                to get a quick overview of your topic.
-            </GuidanceText>
+                    <Tooltip 
+                        text='Replace R, A, S, G and V with some of your points to see if it has a good flow'
+                        style={{color: 'gray'}}
+                        type='info'
+                    />
+                </GuidanceText>
+            
+                <GuidanceText>
+                    You should also try to give your argument a quick summary/overview in the statement field. This will allow your readers
+                    to get a quick overview of your topic.
+                </GuidanceText>
 
 
-            <CreateArgument history={props.history}/>
-        </ReactCSSTransitionGroup>
+                <CreateArgument history={props.history}/>
+            </ReactCSSTransitionGroup>
+        </Page>
     )
 }
 
