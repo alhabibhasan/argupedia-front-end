e16992b584c0537c62470b670a824eaca1bcc653
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-01 22:31:33 +0000

    Create ExpandCollapse.js

---
+++ src/components/atoms/ExpandCollapse.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+
+const ExpandCollapse = (props) => {
+    const [open, setOpen] = useState(false)
+
+    const renderClose = () => {
+        return (
+            <span onClick={() => setOpen(false)}>-</span>
+        )
+    }
+
+    const renderOpen = () => {
+        return (
+            <span onClick={() => setOpen(true)}>+</span>
+        )
+    }
+
+    return (
+        <div>
+            <div>
+                {props.header}
+                { open ? renderClose() : renderOpen()}
+            </div>
+
+
+            {
+                open ? props.render : ''
+            }
+        </div>
+    )
+}
+
+export {
+     ExpandCollapse
+}
