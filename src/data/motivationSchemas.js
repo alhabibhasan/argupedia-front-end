e42f3cfad1ff6ec379a041790611a287290fef47
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-08 18:45:51 +0000

    Create motivationSchemas.js

---
+++ src/data/motivationSchemas.js
@@ -0,0 +1,25 @@
+const motivationSchemas = {
+    argumentFromPositionToKnow:  {
+        label: 'Position to know'
+    },
+    appealToExpertOpinion: {
+        label: 'Expert Opinion'
+    },
+    appealToPopularOpinion: {
+        label: 'Appeal to popular opinion'
+    },
+    argumentFromAnalogy: {
+        label: 'Argument from analogy'
+    },
+    argumentFromCorrelationToCause: {
+        label: 'Argument from correlation to cause'
+    },
+    argumentFromPostiveNegativeConsequences: {
+        label: 'Argument from positive/negative consequence'
+    },
+    slipperySlopeArgument: {
+        label: 'Slippery slope argument'
+    },
+}
+
+export default motivationSchemas
