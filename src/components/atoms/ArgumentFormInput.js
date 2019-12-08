b02aea257aaba7554b8fadf5f35a3e6f109a4b07
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-08 18:46:19 +0000

    Reformat

--- src/components/atoms/ArgumentFormInput.js
+++ src/components/atoms/ArgumentFormInput.js
@@ -4,30 +4,33 @@ import {tooltipErrorRed, formInputErrorRed} from '../../util/colours'
 import Tooltip from '../atoms/Tooltip'
 
 const fieldDecoratorValues = {
-statement: {
-    label: 'Statement',
-    placeholder:'Give a short overview of your argument.'
-},
-circumstance: {
-    label: 'Current circumstance',
-    placeholder: 'Describe the current situation surrounding your argument.'
-},
-action: {
-    label: 'Action',
-    placeholder: 'What action do you recommend happens.'
-},
-newCircumstance: {
-    label: 'New circumstance',
-    placeholder: 'What will the action you suggest result in?'
-},
-goal: {
-    label: 'Achieving the goal of',
-    placeholder: 'What goal would the new situation achieve?'
-},
-value: {
-    label: 'Promoting values of',
-    placeholder: 'What values are now more prevalent due to your suggested action?'
-},
+    statement: {
+        label: 'Statement',
+        placeholder:'Give a short overview of your argument.'
+    },
+    circumstance: {
+        label: 'Current circumstance',
+        placeholder: 'Describe the current situation surrounding your argument.'
+    },
+    action: {
+        label: 'Action',
+        placeholder: 'What action do you recommend happens.'
+    },
+    newCircumstance: {
+        label: 'New circumstance',
+        placeholder: 'What will the action you suggest result in?'
+    },
+    goal: {
+        label: 'Achieving the goal of',
+        placeholder: 'What goal would the new situation achieve?'
+    },
+    value: {
+        label: 'Promoting values of',
+        placeholder: 'What values are now more prevalent due to your suggested action?'
+    },
+    argumentBasis: {
+        label: 'What is the basis of your argument?'
+    }
 }
 
 const getStyles = (form, field) => {
