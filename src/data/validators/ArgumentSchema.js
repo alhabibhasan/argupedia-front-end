127300fe4f5d9c747e8c5429130c7e827453403c
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-11 21:03:53 +0000

    Add response form schema

--- src/data/validators/ArgumentSchema.js
+++ src/data/validators/ArgumentSchema.js
@@ -24,6 +24,32 @@ const ArgumentSchema = Yup.object().shape({
         .notRequired()
     })
 
+const ResponseSchema = Yup.object().shape({
+    statement : Yup.string()
+        .min(5, 'Too short!')
+        .required('Required!'),
+    circumstance : Yup.string()
+        .min(5, 'Too short!')
+        .required('Required!'),
+    action : Yup.string()
+        .min(5, 'Too short!')
+        .required('Required!'),
+    newCircumstance : Yup.string()
+        .min(5, 'Too short!')
+        .notRequired(),
+    goal : Yup.string()
+        .min(5, 'Too short!')
+        .notRequired(),
+    value : Yup.string()
+        .min(5, 'Too short!')
+        .notRequired(),
+    argumentBasis: Yup.string()
+        .required('Required'),
+    sourceList: Yup.string()
+        .notRequired()
+    })
+
 export {
-    ArgumentSchema
+    ArgumentSchema,
+    ResponseSchema
 }
