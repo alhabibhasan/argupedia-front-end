0bb66d887d6fd0542602ad3d89e9e56832e67585
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-11 21:05:16 +0000

    Add groupings to dropdown list component.

--- src/components/atoms/DropdownList.js
+++ src/components/atoms/DropdownList.js
@@ -7,16 +7,34 @@ const DropdownList = (props) => {
         props.setSelectedValue(selected)
     }, [selected])
 
-    const renderFields = () => {
-        return Object.keys(props.valuesToRender).map((property, index) => {
-            return <option key={index} value={property}>{props.valuesToRender[property].label}</option>
+    const renderRegularList = () => {
+        return props.valuesToRender.map((property, index) => {
+            return <option key={index} value={property.id}>{property.label}</option>
+        })
+    }
+
+    const renderCategorizedList = () => {
+        return props.valuesToRender.map((property, i) => {
+            let groupOptions = props.valuesToRender[i][props.categoryOptionsField]
+            let label = props.valuesToRender[i].label
+
+            let optionTags = groupOptions.map((opt, index) => {
+                return <option key={index} value={opt}>{opt}</option>
+            })
+
+            let groupTag = <optgroup key={i} label={label}>
+                {optionTags}
+            </optgroup>
+
+            return groupTag
         })
     }
 
     return (
         <div>
-            <select multiple onChange={(e) => setSelected(e.target.value)}>
-                {renderFields()}  
+            <select multiple={props.multiple} onChange={(e) => setSelected(e.target.value)}>
+                <option value='default'>{props.defaultText ? props.defaultText : 'Select a value'}</option>
+                {props.categorized ? renderCategorizedList() : renderRegularList()}  
             </select>
         </div>
         )
