73c1e75a6eb2088f0403c4017d824781a888e59d
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-16 20:59:35 +0000

    Allow users to repond to arguments.

--- src/components/organisms/Respond.js
+++ src/components/organisms/Respond.js
@@ -5,9 +5,9 @@ import ArgumentForm from './ArgumentForm'
 import { ResponseSchema } from '../../data/validators/ArgumentSchema'
 import { createResponse } from '../../data/api/Api'
 import {withRouter} from 'react-router-dom'
-import { waitThenRedirectTo } from '../../util/redirect'
-import { readArgument } from '../../data/routes'
 import argumentFields from '../../data/argumentFields'
+
+const defaultSuccessMessage = 'Your response was created successfully.'
 
 const Response = (props) => {
     const [selectedPoint, setSelectedPoint] = useState()
@@ -18,9 +18,10 @@ const Response = (props) => {
     const hasCriticalQuestion = () => criticalQuestion && criticalQuestion != 'default'
 
     const renderCriticalQuestions = () => {
+        if (!hasSelectedPoint()) return;
         let copiedSchemes = JSON.parse(JSON.stringify(motivationSchemas))
         let currentCategory = copiedSchemes.filter(schema => schema.id === props.root.argumentBasis)[0]
-        if (!currentCategory.label.includes('Recommended')) {
+        if (currentCategory && !currentCategory.label.includes('Recommended')) {
             copiedSchemes[copiedSchemes.indexOf(currentCategory)].label = currentCategory.label + ' - Recommended '
         }
         return <DropdownList 
@@ -31,19 +32,19 @@ const Response = (props) => {
     }
 
     const getQuestionCategoryLabel = (value) => {
-        return motivationSchemas.filter(schema => schema.id === value)[0].label
+        let schema = motivationSchemas.filter(schema => schema.id === value)[0]
+        if (schema && schema.label) return schema.label
+        return 'Category Label not found'
     }
 
     const renderSelectedPoint = () => {
-        if (hasSelectedPoint(selectedPoint) ) {
+        if (hasSelectedPoint() ) {
             return (
                 <div>
                     <div>
-                        <div>
-                            We recommend using questions from the "{getQuestionCategoryLabel(props.root.argumentBasis)}" list.
-                        </div>  
-                        {renderCriticalQuestions()}
-                    </div>
+                        We recommend using questions from the "{getQuestionCategoryLabel(props.root.argumentBasis)}" list.
+                    </div>  
+                    {renderCriticalQuestions()}
                 </div>
             )
         }
@@ -62,30 +63,41 @@ const Response = (props) => {
     }
 
     const renderResponseForm = () => {
-        if (hasSelectedPoint() && hasCriticalQuestion()) {
-            return <ArgumentForm 
-                schema={ResponseSchema} 
-                statement={getQuote()}
-                onSubmit={(values, setArgumentStatus, setArgumentStatusMessage) => {
-                    let valuesCopy = JSON.parse(JSON.stringify(values))
-                    valuesCopy['root'] = false
-                    valuesCopy['propertyToRespondTo'] = selectedPoint
-                    valuesCopy['rootId'] = props.root.id
-                    createResponse(props.root.id, valuesCopy)
-                    .then(createdNode => {
-                        setArgumentStatus('SUCCESS')
-                        setArgumentStatusMessage('Your response was created successfully.')
-                        waitThenRedirectTo(props.history, readArgument.use + props.root.id, 1500)
-                    })
-                    .catch(() => {
-                        setArgumentStatus('ERROR')
+        if (!hasSelectedPoint() || !hasCriticalQuestion()) return;
+
+        return <ArgumentForm
+            hideBack={props.hideBack} 
+            schema={ResponseSchema} 
+            statement={getQuote()}
+            onSubmit={(values, setArgumentStatus, setArgumentStatusMessage) => {
+                let valuesCopy = JSON.parse(JSON.stringify(values))
+                valuesCopy['root'] = false
+                valuesCopy['propertyToRespondTo'] = selectedPoint
+                valuesCopy['rootId'] = props.root.id
+                createResponse(props.root.id, valuesCopy)
+                .then(() => {
+                    setArgumentStatus('SUCCESS')
+                    setArgumentStatusMessage(props.successMessage ? props.successMessage : defaultSuccessMessage)
+                    setTimeout(() => {
+                        setArgumentStatus('NOT_ATTEMPTED')
+                        window.scrollTo({
+                            top: 0,
+                            behavior: 'smooth'
+                        })
                         setTimeout(() => {
-                            setArgumentStatus('NOT_ATTEMPTED')
-                        }, 1500)
-                        setArgumentStatusMessage('Oops, there was an error, please try again. If it persists, contact the admin via admin@argupedia.com.')
-                    })
-                }}/>
-        }
+                            props.updateArgument()
+                        }, 1000)
+                    }, 1000)
+                })
+                .catch((err) => {
+                    console.log(err)
+                    setArgumentStatus('ERROR')
+                    setTimeout(() => {
+                        setArgumentStatus('NOT_ATTEMPTED')
+                    }, 1500)
+                    setArgumentStatusMessage('Oops, there was an error, please try again. If it persists, contact the admin via admin@argupedia.com.')
+                })
+            }}/>
     }
 
     return (
