531c0c51cbf2c7d248f6b14b3ed71456da3b3f0a
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-15 19:29:44 +0000

    Add auth routes and paths

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import {
   BrowserRouter as Router,
@@ -10,24 +10,42 @@ import {
 import './App.css'
 import Index from './components/pages/Index'
 import RootArgs from './components/pages/RootArgs'
-import ArgChain from './components/pages/ArgChain'
+import ViewArgument from './components/pages/ViewArgument'
+import LoginView from './components/pages/Auth/Login'
+import LogoutView from './components/pages/Auth/Logout'
+import PasswordResetView from './components/pages/Auth/PasswordReset'
+import RegisterView from './components/pages/Auth/Register'
+
 
 import Navigation from './components/organisms/Nav'
 import Footer from './components/organisms/Footer'
 import CreateArgument from './components/pages/CreateArgument'
 
-import {index, roots, createArgument} from './data/routes'
+import {index, roots, createArgument, auth} from './data/routes'
+import authListener from './data/auth/auth-listener'
 
 function App() {
+  const [user, setUser] = useState(null)
+  const [checkingAuth, setCheckingAuth] = useState(true)
+
+  useEffect(() => {
+    authListener(setUser, setCheckingAuth)
+  })
+
   return (
     <Router>
       <div className='App'>
-        <Navigation/>
+        <Navigation user={user}/>
+        {user ? user.displayName : <span></span> }
         <Switch>
-          <Route exact path={roots.defintion} render={(props) => <RootArgs {...props} />}></Route>
-          <Route exact path='/argument/:id(\d+)' render={(props) => <ArgChain {...props}/>}></Route>
-          <Route exact path={createArgument.defintion} render={(props) => <CreateArgument {...props}/>}></Route>
-          <Route exact path={index.defintion} render={(props) => <Index {...props} />}></Route>
+          <Route exact path={roots.defintion} render={(props) => <RootArgs user={user} {...props} />}></Route>
+          <Route exact path='/argument/:id(\d+)' render={(props) => <ViewArgument user={user} {...props}/>}></Route>
+          <Route exact path={createArgument.defintion} render={(props) => <CreateArgument user={user} {...props}/>}></Route>
+          <Route exact path={index.defintion} render={(props) => <Index user={user} {...props} />}></Route>
+          <Route exact path={auth.login.use} render={(props) => <LoginView user={user} {...props} ></LoginView>}></Route>
+          <Route exact path={auth.logout.use} render={(props) => <LogoutView user={user} {...props} ></LogoutView>}></Route>
+          <Route exact path={auth.register.use} render={(props) => <RegisterView user={user} {...props} ></RegisterView>}></Route>
+          <Route exact path={auth.passwordReset.use} render={(props) => <PasswordResetView user={user} {...props} ></PasswordResetView>}></Route>
           <Route render={() => <Redirect to={index.defintion} />}></Route>
         </Switch>
         <Footer/>
--- src/data/routes.js
+++ src/data/routes.js
@@ -2,11 +2,26 @@ const index = { defintion: '/', use: '/'}
 const roots = { defintion: '/roots', use: '/roots'}
 const createArgument = { defintion: '/argument/create', use: '/argument/create'}
 const readArgument = { defintion: '/argument/:id', use: '/argument/'}
+const auth = {
+    login: {
+        use: '/login'
+    },
+    logout: {
+        use: '/logout'
+    },
+    register: {
+        use: '/register'
+    },
+    passwordReset: {
+        use: '/password-reset'
+    }
+}
 
 
 export {
     index,
     roots,
     createArgument,
-    readArgument
+    readArgument,
+    auth,
 }
