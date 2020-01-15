b273557d73e70d0e2ee823324c3acef562171ae8
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-15 19:31:11 +0000

    Add account options to nav

--- src/components/organisms/Nav.js
+++ src/components/organisms/Nav.js
@@ -6,30 +6,59 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  Dropdown, 
+  DropdownItem, 
+  DropdownToggle, 
+  DropdownMenu,
 } from 'reactstrap'
 import {redirectTo} from '../../util/redirect'
 import { withRouter } from 'react-router-dom'
-import {index, roots} from '../../data/routes'
+import {index, roots, auth} from '../../data/routes'
+import { userLoggedInAndEmailVerified } from '../../data/auth/user-checks'
 
 const Navigation = (props) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [accountDropdownOpen, setAccountDropdown] = useState(false)
 
-  const toggle = () => setIsOpen(!isOpen)
+  const toggleNavOpen = () => setIsOpen(!isOpen)
+  const toggleAccountDropdown = () => setAccountDropdown(!accountDropdownOpen)
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
+      <Navbar color="light" light expand="lg">
         <NavbarBrand onClick={() => redirectTo(props.history, index.use)}>Argupedia</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+        <NavbarToggler onClick={toggleNavOpen} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
               <NavLink onClick={() => redirectTo(props.history, index.use)}>Home</NavLink>
             </NavItem>
             <NavItem>
-            <NavLink onClick={() => redirectTo(props.history, roots.use)}>View roots</NavLink>
+              <NavLink onClick={() => redirectTo(props.history, roots.use)}>View roots</NavLink>
             </NavItem>
+
+            {userLoggedInAndEmailVerified(props.user) ? 
+              <Dropdown nav isOpen={accountDropdownOpen} toggle={toggleAccountDropdown}>
+                <DropdownToggle nav caret>
+                  Account
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem onClick={() => redirectTo(props.history, auth.logout.use)}>Sign out</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+            :
+            <Dropdown nav isOpen={accountDropdownOpen} toggle={toggleAccountDropdown}>
+              <DropdownToggle nav caret>
+                Account
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={() => redirectTo(props.history, auth.login.use)}>Sign in</DropdownItem>
+                <DropdownItem onClick={() => redirectTo(props.history, auth.register.use)}>Register</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            }
+            
           </Nav>
         </Collapse>
       </Navbar>
