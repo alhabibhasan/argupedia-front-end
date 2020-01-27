d832193c0b3118c3c7d8566c24f8216a7c2b7f54
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-27 17:28:22 +0000

    Move arg form config to separate file.

---
+++ src/components/molecules/FormInput/ArgumentFormInput/Config.js
@@ -0,0 +1,31 @@
+const argumentFormConfig = {
+    statement: {
+        label: 'Statement',
+        placeholder:'Give a short overview of your argument.'
+    },
+    circumstance: {
+        label: 'Current circumstance',
+        placeholder: 'Describe the current situation surrounding your argument.'
+    },
+    action: {
+        label: 'Action',
+        placeholder: 'What action do you recommend happens.'
+    },
+    newCircumstance: {
+        label: 'New circumstance',
+        placeholder: 'What will the action you suggest result in?'
+    },
+    goal: {
+        label: 'Achieving the goal of',
+        placeholder: 'What goal would the new situation achieve?'
+    },
+    value: {
+        label: 'Promoting values of',
+        placeholder: 'What values are now more prevalent due to your suggested action?'
+    },
+    argumentBasis: {
+        label: 'What is the basis of your argument?'
+    }
+}
+
+export default argumentFormConfig
