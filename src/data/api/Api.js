b600f25950cae7521f2a2a285b038e6a65443ab8
Muhammed Hasan <muhammedahasan@outlook.com>
2020-02-08 14:45:18 +0000

    Explicitly check parentId length before sending update request.

--- src/data/api/Api.js
+++ src/data/api/Api.js
@@ -18,7 +18,7 @@ const createResponse = (rootId, arg) => {
 
 const updateArgument = (argId, arg) => {
     let updateEndpoint = process.env.REACT_APP_API_BASE_URL+ '/arg/update/'
-    if (arg.parentId) {
+    if (arg.parentId > 0) {
         updateEndpoint += 'response/'
     }
     return axios.patch(updateEndpoint + argId, arg)
