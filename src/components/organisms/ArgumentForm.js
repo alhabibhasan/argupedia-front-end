faa486fbb39513b1cf372bdbdc3709d780251b31
Muhammed Hasan <muhammedahasan@outlook.com>
2020-03-25 00:43:13 +0000

    Refactor, remove formik and use custom form
    We can now have different form layouts depending on which argument basis you select

--- src/components/organisms/ArgumentForm.js
+++ src/components/organisms/ArgumentForm.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect} from 'react'
-import { Formik, Form, Field } from 'formik'
 import Button from '../atoms/Button'
-import ListFormInput from '../molecules/FormInput/ListFormInput'
-import DropdownFormInput from '../molecules/FormInput/DropdownFormInput'
-import {ArgumentSchema} from '../../data/validators/ArgumentSchema'
-import {ArgumentFormInput} from  '../molecules/FormInput/ArgumentFormInput/ArgumentFormInput'
-import { Prompt } from 'react-router-dom'
 
 import './Styles/CreateArgs.scss'
 import {getSchemes} from '../../data/motivationSchemas'
+import { DropdownList } from '../atoms/DropdownList'
+import SourceList from '../molecules/SourceList/SourceList'
+import { camelize } from '../../util/formatting'
 
 const ArgumentForm = (props) => {
   const argStatusValues = {
@@ -17,108 +14,127 @@ const ArgumentForm = (props) => {
     'ERROR' : 'error'
   }
 
-  const LEAVE_MESSAGE = 'If you leave, you will loose any content you have added below. Are you sure you want to leave?'
-
   const [argumentStatus, setArgumentStatus] = useState('NOT_ATTEMPTED')
   const [argumentStatusMessage, setArgumentStatusMessage] = useState('')
-  const [argumentBasis, setArgumentBasis] = useState([])
+  const [argumentBasis, setArgumentBasis] = useState(props.arg && props.arg.argumentBasis || '')
+  const [statement, setStatement] = useState(props.arg && props.arg.statement || '')
+  const [argumentSchemes, setArgumentSchemes] = useState([])
+  const [sourceList, setSourceList ] = useState(props.arg && props.arg.sourceList || [])
+  const [form, setForm] = useState(props.arg || {})
 
-  const confirmLeave = (e) => {
-    if (window.confirm(LEAVE_MESSAGE)) {
-        return e
-    } else {
-      e.preventDefault()
-    }
-  }
-
+  /**
+   * Loads the argument basis' with fields etc. from the db
+   */
   useEffect(() => {
     getSchemes()
     .then(schemes => {
-      setArgumentBasis(schemes)
+      setArgumentSchemes(schemes)
     })
-    window.addEventListener("beforeunload" , (e) => confirmLeave(e))
-    return window.removeEventListener("beforeunload", (e) => confirmLeave(e))
   }, [props, props.arg])
 
-  const renderFormElems = (values, touched) => {
-    let formFields = []
-    // Convert object to array, otherwise we cannot cycle through it easily
-    for (var prop in values) {
-      if (Object.prototype.hasOwnProperty.call(values, prop)) {
-          formFields.push(prop)
+  /**
+   * ran when the argument basis changes, it is in charge of updating the form so it reflects the fields
+   * included in the current argument basis' fields.
+   */
+  useEffect(() => {
+    if (argumentSchemes && argumentBasis !== 'default') {
+      let selectedBasisFields = argumentSchemes.filter(scheme => scheme.label === argumentBasis)[0]
+      if (selectedBasisFields) {
+        let updatedForm = {}
+        selectedBasisFields.fields.forEach(field => {
+          updatedForm[camelize(field)] = ''
+        })
+        setForm(updatedForm)
       }
     }
+  }, [argumentBasis])
 
-    let inputFields = formFields.map((value, index) => {
-      if (value === 'sourceList') {
-        return <ListFormInput key={index} label='Extra resources'/>
-      } else if (value === 'argumentBasis') {
-        return (<div key={index}>
-            <DropdownFormInput fieldName='argumentBasis' renderedProperties={argumentBasis}/>
-            <hr/>
-          </div>)
-      } else {
-        return (
-          <div key={index}>
-            <Field 
-              component={ArgumentFormInput}
-              type="text" 
-              name={value} 
-            />
-          </div>
-        )
-      }
-    })
-  
-    return (
-      <div>
-        <Prompt 
-          when={Object.keys(touched).length > 0} 
-          message={LEAVE_MESSAGE}/>
-        <Form>
-          {inputFields}
-          <div className="Form-Buttons">
-            <Button icon={argStatusValues[argumentStatus]}/>
-          </div>
-          <div>
-            {argumentStatusMessage}
-          </div>
-        </Form>
-      </div>
-    )
+  /**
+   * This is ran when an an argument is passed in. it ensure that the form is populated using data from the selected basis and the 
+   * data from the existing argument.
+   */
+  useEffect(() => {
+    if (argumentSchemes.length && argumentBasis !== 'default' && props.arg) {
+      let selectedBasisFields = argumentSchemes.filter(scheme => scheme.label === argumentBasis)[0]
+      let populatedForm = {}
+      selectedBasisFields.fields.forEach(field => {
+        let camelFieldName = camelize(field)
+        populatedForm[camelFieldName] = props.arg[camelFieldName]
+      })
+      setForm(populatedForm)
+    }
+  }, [props.arg, argumentBasis, argumentSchemes])
+
+  const updateForm = (field, value) => {
+    let formCopy = JSON.parse(JSON.stringify(form))
+    formCopy[camelize(field)] = value
+    setForm(formCopy)
   }
 
-  const getValue = (field) => {
-    if (props.arg && props.arg[field]) {
-      return props.arg[field]
-    } else {
-      return ''
+  const formSubmit = (evt) => {
+    evt.preventDefault()
+    form['statement'] = statement
+    form['argumentBasis'] = argumentBasis
+    form['sourceList'] = sourceList
+    // need to do validation
+    props.onSubmit(form, setArgumentStatus, setArgumentStatusMessage)
+  }
+
+  const getBasisSpecificFields = () => {
+    if (argumentSchemes) {
+      let selectedBasisFields = argumentSchemes.filter(scheme => scheme.label === argumentBasis)[0]
+      if (selectedBasisFields) {
+        let formElements = selectedBasisFields.fields.map((field, index) => {
+          return (
+            <p key={index}>
+              <label htmlFor={field}>
+                {field}
+                <br/>
+                <textarea value={form[camelize(field)]} onChange={e => updateForm(field, e.target.value)}/>
+              </label>
+            </p>
+          )
+        })
+        return formElements
+      }
     }
   }
   
-  return (
-    <div className='Create-Arg-Form'>
-      <Formik
-      initialValues={{ 
-        statement: getValue('statement') || props.statement,
-        argumentBasis: getValue('argumentBasis'),
-        circumstance: getValue('circumstance'),
-        action: getValue('action'),
-        newCircumstance: getValue('newCircumstance'),
-        goal: getValue('goal'),
-        value: getValue('value'),
-        sourceList: getValue('sourceList'),}}
-      enableReinitialize={true}
-      validationSchema={props.schema ? props.schema : ArgumentSchema}
-      onSubmit={(values, formikBag) => {
-        formikBag.setSubmitting(true)
-        props.onSubmit(values, setArgumentStatus, setArgumentStatusMessage)
-      }}
-      >
-        {({values, touched}) => renderFormElems(values, touched)}
-      </Formik>
-    </div>
-  )
+  const renderForm = () => {
+    let otherFields = getBasisSpecificFields()
+    let formElem = <form onSubmit={e => formSubmit(e)}>
+      <p>
+        <label htmlFor='statement'>Statement
+          <br/>
+          <textarea name='statement' value={statement} onChange={e => setStatement(e.target.value)}/>
+        </label>
+      </p>
+      {props.edit ? '' : 
+        <div>
+          <label>
+            Argument Scheme
+            <DropdownList valuesToRender={argumentSchemes} selectedValue={argumentBasis} setSelectedValue={setArgumentBasis}/>
+          </label>
+        </div>
+      }
+      {otherFields}
+      {argumentBasis !== 'default' ?  
+        <div>
+          <SourceList label={'Sources'} list={JSON.stringify(sourceList)}
+            updateList={stringifiedList => setSourceList(JSON.parse(stringifiedList))}/>
+          <div className="Form-Buttons" onClick={e => formSubmit(e)}>
+                <Button icon={argStatusValues[argumentStatus]}/>
+          </div>
+        </div> 
+        : 
+        ''}
+      <br/>
+      {argumentStatusMessage}
+    </form>
+    return formElem
+  }
+
+  return <div className='Create-Arg-Form'> {renderForm()} </div>
 }
 
 export default ArgumentForm
