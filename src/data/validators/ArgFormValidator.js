2f07ee2c766263891101f028db6b3bcb97e3301f
Muhammed Hasan <muhammedahasan@outlook.com>
2020-03-25 12:34:01 +0000

    Add custom validation rules for each field

--- src/data/validators/ArgFormValidator.js
+++ src/data/validators/ArgFormValidator.js
@@ -1,5 +1,24 @@
 import { camelCaseToSentenceCase } from "../../util/formatting"
 
+const DEFAULT_FIELD_REQUIRED_SIZE = 6
+
+const argumentRules = {
+    'statement': (statement, validationResults) => {
+        if (statement.length < 6) {
+            validationResults.isValid = false
+            validationResults.msg = 'Statement is too short'
+            return validationResults
+        }
+    },
+    'default': (fieldValue, fieldName, validationResults) => {
+        if (fieldValue.length < DEFAULT_FIELD_REQUIRED_SIZE) {
+            validationResults.isValid = false
+            validationResults.msg = camelCaseToSentenceCase(fieldName) + ' is too short.'
+            return validationResults
+        } 
+    }
+}
+
 const validateArgFormNotEmpty = (form, optionalFields) => {
     let validationResults = {
         isValid: true,
@@ -10,6 +29,17 @@ const validateArgFormNotEmpty = (form, optionalFields) => {
             validationResults.isValid = false
             validationResults.msg = 'You are missing the ' + camelCaseToSentenceCase(objectProperty) + ' field.'
             return validationResults
+        }
+        if (!optionalFields.includes(objectProperty)) {
+            let fieldRule = argumentRules[objectProperty]
+            let customValidationResults
+            if (fieldRule) {
+                customValidationResults = fieldRule(form[objectProperty], validationResults)
+            } else {
+                fieldRule = argumentRules['default']
+                customValidationResults = fieldRule(form[objectProperty], objectProperty, validationResults)
+            }
+            if (!validationResults.isValid) return validationResults
         }
     }
     return validationResults
