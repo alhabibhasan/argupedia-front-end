e885de5703bf2cde43fa720e1fec653c749d1d4a
Muhammed Hasan <muhammedahasan@outlook.com>
2020-03-25 00:41:19 +0000

    Add camel case generator

--- src/util/formatting.js
+++ src/util/formatting.js
@@ -7,4 +7,18 @@ const camelCaseToSentenceCase = string => {
     return finalResult
 }
 
-export default camelCaseToSentenceCase
+/**
+ * Source: https://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case/37041217
+ * @param {*} str 
+ */
+const camelize = (str) => {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+      if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+      return index === 0 ? match.toLowerCase() : match.toUpperCase();
+    });
+  }
+
+export {
+    camelCaseToSentenceCase,
+    camelize
+}
